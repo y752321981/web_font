6c3885d0f631435ba7f62495f2baaa9da2f77a0d
y752321981 <752321981@qq.com>
2022-04-10 22:32:56 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './routers/index'
+createApp(App).use(router).mount('#app')
+createApp(App).config.productionTip = false
---
+++ src/routers/index.js
@@ -0,0 +1,14 @@
+//引入依赖及组件
+import {createRouter, createWebHistory} from "vue-router";
+
+//设置组件映射规则
+const routes = [
+  
+]
+
+const router = new createRouter({
+  history: createWebHistory(),
+  routes
+})
+//导出
+export default router;
